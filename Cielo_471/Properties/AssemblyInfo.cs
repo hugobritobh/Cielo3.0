c6a7ab038953f19ffb5b33cdfe87375459244b59
Hugo de Brito V. R. Alves <hugobritobh@gmail.com>
2020-07-02 18:43:11 -0300

    Adicionado uma versao especifica para o 4.7.1 Versão 1.0.14

--- Cielo_471/Properties/AssemblyInfo.cs
+++ Cielo_471/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8484ea1f-a623-493b-8da5-8126e291b8ea")]
+[assembly: Guid("087d8081-f316-4780-a168-18038cbb2479")]
 
 // Version information for an assembly consists of the following four values:
 //
